d97b980eefb5b07b273769ee13d176d49b181b27
Selma <anaselma1962@gmail.com>
2019-07-18 14:03:14 -0300

    include and test exchange controller

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,7 +11,7 @@
 // about supported directives.
 //
 //= require jquery
-//= require bootstrap/dis/js/bootstrap
+//= require bootstrap/dist/js/bootstrap
 //= require rails-ujs
 //= require activestorage
 //= require turbolinks
